441348941dac76ad1168dd6b6940b05cb2842e3f
WillieXia <51302855+WillieXia@users.noreply.github.com>
2020-02-29 16:28:31 -0500

    NavBarDone

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React, { Component } from 'react';
 import NavBar from './components/NavBar';
+import Background from './squat-pic.jpg';
 
 class App extends React.Component {
   render(){
     return(
       <div className = "App">
-        <div classname="main_Background"></div>
+        <div style={{  
+          backgroundImage: Background,
+          backgroundPosition: 'center',
+          backgroundSize: 'cover',
+          backgroundRepeat: 'no-repeat',
+          height: "1080px",
+          width: "1920px"
+        }}>
+        </div>
+        <img src="./squat-pic.jpg"/>
         <NavBar />
         <main style={{marginTop: '100px'}}>          
           <p>This is the page content!</p>
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,17 +1,16 @@
 import React from 'react';
-import {Text, StyleSheet} from 'react-native';
 import './NavBar.css';
 
 const NavBar = props =>(
     <header className="navbar">
         <nav className="navbar_navigation">
             <div></div>
-            <div className="navBar_logo"><a href="/"><Text style={{fontWeight: 'bold'}}>AI</Text> Sports Analytics</a></div>
+            <div className="navBar_logo"><a href="/">AI Sports Analytics</a></div>
             <div className ="spacer"></div>
             <div className="navbar_items">
                 <ul>
                     <li className="Item1"><a href="/">The App</a></li>
-                    <li className="Item2"><a href="/"></a>Login</li>
+                    <li className="Item2"><a href="/">Login</a></li>
                 </ul>
             </div>
         </nav>
