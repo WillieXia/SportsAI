a7faa03b850074b399b3f8398ede178358c88942
WillieXia <51302855+WillieXia@users.noreply.github.com>
2020-03-03 17:06:11 -0500

    image-added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
 import React, { Component } from 'react';
+import './App.css';
 import NavBar from './components/NavBar';
-import Background from './squat-pic.jpg';
+import imgone from './squat-pic2.jpg';
 
 class App extends React.Component {
   render(){
     return(
       <div className = "App">
-        <div style={{  
-          backgroundImage: Background,
-          backgroundPosition: 'center',
-          backgroundSize: 'cover',
-          backgroundRepeat: 'no-repeat',
-          height: "1080px",
-          width: "1920px"
-        }}>
-        </div>
-        <img src="./squat-pic.jpg"/>
+        <img className = "backgroundSquat"  src={imgone} />
         <NavBar />
-        <main style={{marginTop: '100px'}}>          
-          <p>This is the page content!</p>
-        </main>
       </div>
     );
   }
