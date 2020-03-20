007a962818224b45df90496f317eb7289787e73c
WillieXia <WillieXia@users.noreply.github.com>
2020-03-20 16:01:53 -0400

    initializing routing

--- src/App.js
+++ src/App.js
@@ -1,78 +1,87 @@
 import React, { Component } from 'react';
 import './App.css';
 import NavBar from './components/NavBar';
+
+
+
 import baller from './components/images/hooper.png';
-import styled, {keyframes} from '../node_modules/styled-components';
+import Radium, {StyleRoot} from '../node_modules/radium';
 import {bounce} from '../node_modules/react-animations';
+import {slideInRight} from '../node_modules/react-animations/lib/slide-in-right';
 
-const Bounce = styled.div`animation: 2s ${keyframes`${bounce}`} infinite`;
-
-class App extends React.Component {
-  render() {
-    return (
-      <div className="App">
-        <meta name="viewport" content="width=device-width, maximum-scale=0" />
-        <section className="nbar">
-          <NavBar />
-        </section>
-        <section className="CatchSection">
-          <header className="Catch">
-            {/*Information part*/}
-            <section className="leftSection">
-              <header className="Title1">
-                Revolutionizing the
-              </header>
-              <header className="Title2" >
-                <span className="SportsWord">Sports</span> Industry
-              </header>
-              <p className="Description">
-                We teach machines to see and solve hard <br />problems across the sports industry.
-              </p>
-            </section>
-            <section className="rightSection">
-              <img className="ballerimage" src={baller} />
-            </section>
-          </header>
-        </section>
-        <header className="Description2">
-          <header className="Artificial_Intelligence_in_Sports">
-            Artificial Int<span className="ellige">ellige</span>nce in Sports
-            <p className="The_world_of">
-              The world of sports is abundant in such quantifiable elements,
-              making it ideal for the use of artificial intelligence.
-              It has brought positive impact in the following areas:
-            </p>
-            <div className="CircleImages">
-              <img src={baller} alt="Avatar"></img>
-              <div className="spacer2"></div>
-              <img src={baller} alt="Avatar"></img>
-              <div className="spacer2"></div>
-              <img src={baller} alt="Avatar"></img>
-            </div>
-            <p></p>
-          </header>
-        </header>
-        <section className="EndSection">
-          <section className="leftSection">
-            <p className="See_it_in_action">
-              See it in action
-            </p>
-            <header className="How_it_Works">
-              <span className="How_it"> How it</span> Works
-            </header>
-            <p className="Description3">
-              DESCRIPTION DESCRIPTION DESCRIPTION DESCRIPTION DESCRIPTION DESCRIPTION DESCRIPTION DESCRIPTION
-              DESCRIPTION DESCRIPTION DESCRIPTION DESCRIPTION DESCRIPTION DESCRIPTION DESCRIPTION DESCRIPTION
-              DESCRIPTION DESCRIPTION DESCRIPTION DESCRIPTION DESCRIPTION DESCRIPTION DESCRIPTION DESCRIPTION
-              DESCRIPTION DESCRIPTION DESCRIPTION DESCRIPTION DESCRIPTION DESCRIPTION DESCRIPTION DESCRIPTION
-            </p>
-          </section>
-          <section className ="RightSection">
-            <img className = "AI-Photo" src = {baller}/>
-          </section>
-        </section>
-      </div>
-    );
+const styles = {
+  bounce: {
+    animation: 'x 1s',
+    animationName: Radium.keyframes(bounce, 'bounce')
   }
 }
-export default App;
+
+  class App extends React.Component {
+    render() {
+      return (
+        <div className="App">
+          <meta name="viewport" content="width=device-width, maximum-scale=0" />
+          <section className="nbar">
+              <NavBar />
+          </section>
+          <section className="CatchSection">
+            <header className="Catch">
+              {/*Information part*/}
+              <section className="leftSection">
+                <header className="Title1">
+                  Revolutionizing the
+                </header>
+                <header className="Title2" >
+                  <span className="SportsWord">Sports</span> Industry
+                </header>
+                <p className="Description">
+                  We teach machines to see and solve hard <br />problems across the sports industry.
+                </p>
+              </section>
+              <section className="rightSection">
+                <img className="ballerimage" src={baller} />
+              </section>
+            </header>
+          </section>
+          <section className="Description2">
+            <div className="Artificial_Intelligence_in_Sports">
+              <header>Artificial Int<span className="ellige">ellige</span>nce in Sports</header>
+              <p className="The_world_of">
+                The world of sports is abundant in such quantifiable elements,
+                making it ideal for the use of artificial intelligence.
+                It has brought positive impact in the following areas:
+              </p>
+              <div className="CircleImages">
+                <img src={baller} alt="Avatar"></img>
+                <div className="spacer2"></div>
+                <img src={baller} alt="Avatar"></img>
+                <div className="spacer2"></div>
+                <img src={baller} alt="Avatar"></img>
+              </div>
+              <p></p>
+            </div>
+          </section>
+          <section className="EndSection">
+            <section className="leftSection">
+              <p className="See_it_in_action">
+                See it in action
+              </p>
+              <header className="How_it_Works">
+                <span className="How_it"> How it</span> Works
+              </header>
+              <p className="Description3">
+                DESCRIPTION DESCRIPTION DESCRIPTION DESCRIPTION DESCRIPTION DESCRIPTION DESCRIPTION DESCRIPTION
+                DESCRIPTION DESCRIPTION DESCRIPTION DESCRIPTION DESCRIPTION DESCRIPTION DESCRIPTION DESCRIPTION
+                DESCRIPTION DESCRIPTION DESCRIPTION DESCRIPTION DESCRIPTION DESCRIPTION DESCRIPTION DESCRIPTION
+                DESCRIPTION DESCRIPTION DESCRIPTION DESCRIPTION DESCRIPTION DESCRIPTION DESCRIPTION DESCRIPTION
+              </p>
+            </section>
+            <section className ="RightSection">
+              <img className = "AI-Photo" src = {baller}/>
+            </section>
+          </section>
+        </div>
+      );
+    }
+  }
+  export default App;
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,7 +1,9 @@
-import React from 'react';
-import './NavBar.css';  
+import React, { Component } from 'react';
+import './NavBar.css';
+import { BrowserRouter as Router, Route, Link} from '../../node_modules/react-router-dom';
+import Login from '../Login';
 
-const NavBar = props =>(
+const NavBar = () =>( 
     <header className="navbar">
         <nav className="navbar_navigation">
             <div></div>
@@ -9,14 +11,17 @@ const NavBar = props =>(
             <div className ="spacer"></div>
             <div className="navbar_items">
                 <ul>
-                    <li className="Item1"><a href="/">About</a></li>
-                    <li className="Item2"><a href="/">Sports</a></li>
-                    <li className="Item3"><a href="/">Team</a></li>
-                    <li className="Item4"><a href="/">Contact</a></li>
+                    <Router>
+                            <li className="Item1"><Link to="/Login">Login</Link></li>
+                            <li className="Item2"><a href="/">About</a></li>
+                            <li className="Item3"><a href="/">Team</a></li>
+                            <li className="Item4"><a href="/">Contact</a></li>
+                        <Route path='/Login' component={Login} />
+
+                    </Router>
                 </ul>
             </div>
         </nav>
     </header>
 );
-
 export default NavBar;
