c19a263eb76854d3ae5d860ea2e802b09b4553b0
WillieXia <WillieXia@users.noreply.github.com>
2020-03-20 18:50:00 -0400

    added routing

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
 import NavBar from './components/NavBar';
+import { BrowserRouter as Router, Route, Redirect } from '../node_modules/react-router-dom';
 
 
 
 import baller from './components/images/hooper.png';
-import Radium, {StyleRoot} from '../node_modules/radium';
-import {bounce} from '../node_modules/react-animations';
-import {slideInRight} from '../node_modules/react-animations/lib/slide-in-right';
+import Radium, { StyleRoot } from '../node_modules/radium';
+import { bounce } from '../node_modules/react-animations';
+import { slideInRight } from '../node_modules/react-animations/lib/slide-in-right';
 
 const styles = {
   bounce: {
@@ -16,72 +17,120 @@ const styles = {
   }
 }
 
-  class App extends React.Component {
-    render() {
-      return (
-        <div className="App">
-          <meta name="viewport" content="width=device-width, maximum-scale=0" />
-          <section className="nbar">
-              <NavBar />
-          </section>
-          <section className="CatchSection">
-            <header className="Catch">
-              {/*Information part*/}
-              <section className="leftSection">
-                <header className="Title1">
-                  Revolutionizing the
-                </header>
-                <header className="Title2" >
-                  <span className="SportsWord">Sports</span> Industry
-                </header>
-                <p className="Description">
-                  We teach machines to see and solve hard <br />problems across the sports industry.
-                </p>
-              </section>
-              <section className="rightSection">
-                <img className="ballerimage" src={baller} />
-              </section>
-            </header>
-          </section>
-          <section className="Description2">
-            <div className="Artificial_Intelligence_in_Sports">
-              <header>Artificial Int<span className="ellige">ellige</span>nce in Sports</header>
-              <p className="The_world_of">
-                The world of sports is abundant in such quantifiable elements,
-                making it ideal for the use of artificial intelligence.
-                It has brought positive impact in the following areas:
-              </p>
-              <div className="CircleImages">
-                <img src={baller} alt="Avatar"></img>
-                <div className="spacer2"></div>
-                <img src={baller} alt="Avatar"></img>
-                <div className="spacer2"></div>
-                <img src={baller} alt="Avatar"></img>
-              </div>
-              <p></p>
-            </div>
-          </section>
-          <section className="EndSection">
-            <section className="leftSection">
-              <p className="See_it_in_action">
-                See it in action
-              </p>
-              <header className="How_it_Works">
-                <span className="How_it"> How it</span> Works
-              </header>
-              <p className="Description3">
-                DESCRIPTION DESCRIPTION DESCRIPTION DESCRIPTION DESCRIPTION DESCRIPTION DESCRIPTION DESCRIPTION
-                DESCRIPTION DESCRIPTION DESCRIPTION DESCRIPTION DESCRIPTION DESCRIPTION DESCRIPTION DESCRIPTION
-                DESCRIPTION DESCRIPTION DESCRIPTION DESCRIPTION DESCRIPTION DESCRIPTION DESCRIPTION DESCRIPTION
-                DESCRIPTION DESCRIPTION DESCRIPTION DESCRIPTION DESCRIPTION DESCRIPTION DESCRIPTION DESCRIPTION
-              </p>
-            </section>
-            <section className ="RightSection">
-              <img className = "AI-Photo" src = {baller}/>
-            </section>
-          </section>
-        </div>
-      );
-    }
+
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      name: 'React'
+    };
   }
-  export default App;
+
+  render(){
+    return (
+      <Router>
+        <NavBar/>
+        <Redirect from="/" to="Home" />
+        <Route path='/Home' component={Home} />
+        <Route path='/Login' component={Login} />
+        <Route path='/About' component={About}/>
+        <Route path='/Team' component={Team} />
+        <Route path='/Contact' component={Contact}/>
+      </Router>
+    );
+  }
+}
+
+const Home = () => (
+  <div>
+    <meta name="viewport" content="width=device-width, maximum-scale=0" />
+    <section className="CatchSection">
+      <header className="Catch">
+        {/*Information part*/}
+        <section className="leftSection">
+          <header className="Title1">
+            Revolutionizing the
+                </header>
+          <header className="Title2" >
+            <span className="SportsWord">Sports</span> Industry
+                </header>
+          <p className="Description">
+            We teach machines to see and solve hard <br />problems across the sports industry.
+                </p>
+        </section>
+        <section className="rightSection">
+          <img className="ballerimage" src={baller} />
+        </section>
+      </header>
+    </section>
+    <section className="Description2">
+      <div className="Artificial_Intelligence_in_Sports">
+        <header>Artificial Int<span className="ellige">ellige</span>nce in Sports</header>
+        <p className="The_world_of">
+          The world of sports is abundant in such quantifiable elements,
+          making it ideal for the use of artificial intelligence.
+          It has brought positive impact in the following areas:
+              </p>
+        <div className="CircleImages">
+          <img src={baller} alt="Avatar"></img>
+          <div className="spacer2"></div>
+          <img src={baller} alt="Avatar"></img>
+          <div className="spacer2"></div>
+          <img src={baller} alt="Avatar"></img>
+        </div>
+        <p></p>
+      </div>
+    </section>
+    <section className="EndSection">
+      <section className="leftSection">
+        <p className="See_it_in_action">
+          See it in action
+              </p>
+        <header className="How_it_Works">
+          <span className="How_it"> How it</span> Works
+              </header>
+        <p className="Description3">
+          DESCRIPTION DESCRIPTION DESCRIPTION DESCRIPTION DESCRIPTION DESCRIPTION DESCRIPTION DESCRIPTION
+          DESCRIPTION DESCRIPTION DESCRIPTION DESCRIPTION DESCRIPTION DESCRIPTION DESCRIPTION DESCRIPTION
+          DESCRIPTION DESCRIPTION DESCRIPTION DESCRIPTION DESCRIPTION DESCRIPTION DESCRIPTION DESCRIPTION
+          DESCRIPTION DESCRIPTION DESCRIPTION DESCRIPTION DESCRIPTION DESCRIPTION DESCRIPTION DESCRIPTION
+              </p>
+      </section>
+      <section className="RightSection">
+        <img className="AI-Photo" src={baller} />
+      </section>
+    </section>
+  </div>
+);
+
+const Login = () => (
+  <div className="Login">
+    <meta name="viewport" content="width=device-width, maximum-scale=0" />
+    <section className="nbar">
+      LOGIN LOGIN LOGIN
+    </section>
+  </div>
+);
+
+const About = () => (
+  <div className="">
+    ABOUT ABOUT ABOUT
+  </div>
+);
+
+const Team = () => (
+  <div className="">
+    TEAM TEAM TEAM
+  </div>
+);
+
+
+const Contact = () => (
+  <div className="">
+    CONTACT CONTACT CONTACT
+  </div>
+);
+
+
+export default App;
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,24 +1,19 @@
 import React, { Component } from 'react';
 import './NavBar.css';
-import { BrowserRouter as Router, Route, Link} from '../../node_modules/react-router-dom';
-import Login from '../Login';
+import { Link } from '../../node_modules/react-router-dom';
 
-const NavBar = () =>( 
+const NavBar = () => (
     <header className="navbar">
         <nav className="navbar_navigation">
             <div></div>
-            <div className="navBar_logo"><a href="/">Static.ai</a></div>
-            <div className ="spacer"></div>
+            <div className="navBar_logo"><Link to="/Home">Static.ai</Link></div>
+            <div className="spacer"></div>
             <div className="navbar_items">
                 <ul>
-                    <Router>
-                            <li className="Item1"><Link to="/Login">Login</Link></li>
-                            <li className="Item2"><a href="/">About</a></li>
-                            <li className="Item3"><a href="/">Team</a></li>
-                            <li className="Item4"><a href="/">Contact</a></li>
-                        <Route path='/Login' component={Login} />
-
-                    </Router>
+                    <li className="Item1"><Link to="/Login">Login</Link></li>
+                    <li className="Item2"><Link to="/About">About</Link></li>
+                    <li className="Item3"><Link to="/Team">Team</Link></li>
+                    <li className="Item4"><Link to="/Contact">Contact</Link></li>
                 </ul>
             </div>
         </nav>
